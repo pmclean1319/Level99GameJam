8af010a6de7769f9982f05992910418481a01bec
j-steve <steve5805@gmail.com>
2023-04-16 20:21:31 -0400

    Moves breathing sound logic from BreathBar to PlayerSounds

--- Assets/Scripts/PlayerSounds.cs
+++ Assets/Scripts/PlayerSounds.cs
@@ -7,55 +7,46 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerFrontCheck))]
 public class PlayerSounds : MonoBehaviour
 {
+
+    [SerializeField] float audioFadeOutDuration = 0.5f;
     [SerializeField] AudioClip walkingFootsteps;
     [SerializeField] AudioClip runningFootsteps;
     [SerializeField] AudioClip jumpSound;
     [SerializeField] AudioClip jumpLandingSound;
+    [SerializeField] AudioClip slowBreathing;
+    [SerializeField] AudioClip normalBreathing;
+    [SerializeField] AudioClip fastBreathing; 
 
     AudioSource footstepsAudioSource;
     AudioSource jumpLandingAudioSource;
+    AudioSource breathingAudioSource;
     PlayerFrontCheck playerFrontCheck;
     PlayerWalk playerWalk;
     PlayerJump playerJump;
 
-    void Start()
-    {
+void Start()
+    { 
         // Setup audio sources.
-        footstepsAudioSource = gameObject.AddComponent<AudioSource>();
-        footstepsAudioSource.loop = true;
-        jumpLandingAudioSource = gameObject.AddComponent<AudioSource>();
+        jumpLandingAudioSource = createAudioSource("JumpLandingAudio", false);
+        footstepsAudioSource = createAudioSource("FootstepsAudio", true);
+        breathingAudioSource = createAudioSource("BreathingAudio", true);
+        footstepsAudioSource.name = "catso";
         // Setup player movement components.
         playerFrontCheck = GetComponent<PlayerFrontCheck>();
         playerWalk = GetComponent<PlayerWalk>();
         playerJump = GetComponent<PlayerJump>();
         playerJump.Jumped += PlayerJumped;
         playerJump.Landed += PlayerLanded;
+        Debug.LogFormat("{0} {1} {2}", jumpLandingAudioSource, footstepsAudioSource, breathingAudioSource);
     }
 
-    void Update()
+    AudioSource createAudioSource(string name, bool loop)
     {
-        ToggleFootstepSounds();
-    }
-
-    void ToggleFootstepSounds()
-    {
-        AudioClip newSound = null;
-        if (playerJump.isEarthBound) {
-            if (playerWalk.isWalking) {
-                newSound = walkingFootsteps;
-            } else if (playerWalk.isRunning) {
-                newSound = runningFootsteps;
-            }
-        }
-        if (newSound != footstepsAudioSource.clip) {
-            if (newSound == null) {
-                footstepsAudioSource.Stop();
-                footstepsAudioSource.clip = null;
-            } else {
-                footstepsAudioSource.clip = newSound;
-                footstepsAudioSource.Play();
-            }
-        }
+        GameObject audioContainer = new GameObject(name);
+        audioContainer.transform.parent = gameObject.transform;
+        AudioSource audioSource = audioContainer.AddComponent<AudioSource>();
+        audioSource.loop = loop;
+        return audioSource;
     }
     void PlayerJumped()
     {
@@ -72,6 +63,48 @@ public class PlayerSounds : MonoBehaviour
             jumpLandingAudioSource.Stop();
             jumpLandingAudioSource.clip = jumpLandingSound;
             jumpLandingAudioSource.Play();
+        }
+    }
+
+    void Update()
+    {
+        ToggleFootstepSounds();
+        ToggleBreathingSounds();
+    }
+
+    void ToggleFootstepSounds()
+    {
+        AudioClip newSound = null;
+        if (playerJump.isEarthBound) {
+            if (playerWalk.isWalking) {
+                newSound = walkingFootsteps;
+            } else if (playerWalk.isRunning) {
+                newSound = runningFootsteps;
+            }
+        }
+        SetSound(footstepsAudioSource, newSound);
+    }
+
+    void ToggleBreathingSounds()
+    {
+        AudioClip newSound = null;
+        if (playerWalk.isRunning) {
+            newSound = fastBreathing;
+        } else if (playerWalk.isWalking) {
+            newSound = normalBreathing;
+        } else  {
+            newSound = slowBreathing;
+        }
+        SetSound(breathingAudioSource, newSound);
+    }
+
+    void SetSound(AudioSource audioSource, AudioClip newSound)
+    {
+        if (newSound != audioSource.clip) {
+            Debug.LogFormat("Fading from {0} to {1}", audioSource.clip, newSound);
+            audioSource.Stop();
+            audioSource.clip = newSound; 
+            audioSource.Play();
         }
     }
 }
