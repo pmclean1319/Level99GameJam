525f35e55736798a9d7343ccf48723661b0ed780
j-steve <steve5805@gmail.com>
2023-04-16 20:50:06 -0400

    Adds fadeout effect to all sounds

--- Assets/Scripts/PlayerSounds.cs
+++ Assets/Scripts/PlayerSounds.cs
@@ -8,14 +8,14 @@ using UnityEngine;
 public class PlayerSounds : MonoBehaviour
 {
 
-    [SerializeField] float audioFadeOutDuration = 0.5f;
+    [SerializeField] float audioFadeOutDuration = 0.15f;
     [SerializeField] AudioClip walkingFootsteps;
     [SerializeField] AudioClip runningFootsteps;
     [SerializeField] AudioClip jumpSound;
     [SerializeField] AudioClip jumpLandingSound;
     [SerializeField] AudioClip slowBreathing;
     [SerializeField] AudioClip normalBreathing;
-    [SerializeField] AudioClip fastBreathing; 
+    [SerializeField] AudioClip fastBreathing;
 
     AudioSource footstepsAudioSource;
     AudioSource jumpLandingAudioSource;
@@ -23,13 +23,15 @@ public class PlayerSounds : MonoBehaviour
     PlayerFrontCheck playerFrontCheck;
     PlayerWalk playerWalk;
     PlayerJump playerJump;
+    Dictionary<AudioSource, AudioClip> queuedSound = new Dictionary<AudioSource, AudioClip>();
+    Dictionary<AudioSource, float> fadeDuration = new Dictionary<AudioSource, float>();
 
-void Start()
-    { 
+    void Start()
+    {
         // Setup audio sources.
-        jumpLandingAudioSource = createAudioSource("JumpLandingAudio", false);
-        footstepsAudioSource = createAudioSource("FootstepsAudio", true);
-        breathingAudioSource = createAudioSource("BreathingAudio", true);
+        jumpLandingAudioSource = createAudioSource("JumpLandingAudio", false, 0f);
+        footstepsAudioSource = createAudioSource("FootstepsAudio", true, 0.15f);
+        breathingAudioSource = createAudioSource("BreathingAudio", true, 0.75f);
         footstepsAudioSource.name = "catso";
         // Setup player movement components.
         playerFrontCheck = GetComponent<PlayerFrontCheck>();
@@ -37,15 +39,15 @@ void Start()
         playerJump = GetComponent<PlayerJump>();
         playerJump.Jumped += PlayerJumped;
         playerJump.Landed += PlayerLanded;
-        Debug.LogFormat("{0} {1} {2}", jumpLandingAudioSource, footstepsAudioSource, breathingAudioSource);
     }
 
-    AudioSource createAudioSource(string name, bool loop)
+    AudioSource createAudioSource(string name, bool loop, float fadeOutDuration)
     {
         GameObject audioContainer = new GameObject(name);
         audioContainer.transform.parent = gameObject.transform;
         AudioSource audioSource = audioContainer.AddComponent<AudioSource>();
         audioSource.loop = loop;
+        fadeDuration[audioSource] = fadeOutDuration;
         return audioSource;
     }
     void PlayerJumped()
@@ -70,6 +72,7 @@ void Start()
     {
         ToggleFootstepSounds();
         ToggleBreathingSounds();
+        UpdateQueuedSounds();
     }
 
     void ToggleFootstepSounds()
@@ -87,12 +90,12 @@ void Start()
 
     void ToggleBreathingSounds()
     {
-        AudioClip newSound = null;
+        AudioClip newSound;
         if (playerWalk.isRunning) {
             newSound = fastBreathing;
         } else if (playerWalk.isWalking) {
             newSound = normalBreathing;
-        } else  {
+        } else {
             newSound = slowBreathing;
         }
         SetSound(breathingAudioSource, newSound);
@@ -100,11 +103,37 @@ void Start()
 
     void SetSound(AudioSource audioSource, AudioClip newSound)
     {
-        if (newSound != audioSource.clip) {
-            Debug.LogFormat("Fading from {0} to {1}", audioSource.clip, newSound);
-            audioSource.Stop();
-            audioSource.clip = newSound; 
+        if (audioSource.clip == null) {
+            audioSource.clip = newSound;
             audioSource.Play();
+        } else if (audioSource.clip == newSound) {
+            if (queuedSound.ContainsKey(audioSource)) {
+                // Sound was fading out -- cancel that.
+                queuedSound.Remove(audioSource);
+                audioSource.volume = 1;
+
+            }
+        } else { // Current clip is wrong; start fadeout.
+            queuedSound[audioSource] = newSound;
         }
     }
+
+    void UpdateQueuedSounds()
+    {
+        List<AudioSource> audioSourcesToRemove = new List<AudioSource>();
+        foreach (AudioSource audioSource in queuedSound.Keys) {
+            float volumeDecrease = Time.deltaTime / fadeDuration[audioSource];
+            audioSource.volume -= volumeDecrease;
+            if (audioSource.volume <= 0) { 
+                audioSourcesToRemove.Add(audioSource);
+            }
+        }
+        foreach (AudioSource audioSource in audioSourcesToRemove) {
+            AudioClip newClip;
+            queuedSound.Remove(audioSource, out newClip);
+            audioSource.volume = 1;
+            audioSource.clip = newClip;
+            audioSource.Play();
+        }
+    } 
 }
