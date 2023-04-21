0eab0a0793ce021e82ac4c5778306068ca2aa9ae
j-steve <steve5805@gmail.com>
2023-04-20 21:06:36 -0400

    adds pause check to jump and walk

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -32,7 +32,7 @@ public class PlayerJump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        {
+        if (!GameState.IsPaused) {
             CheckforFalling();
             JumpBegin();
             //JumpEnd();
